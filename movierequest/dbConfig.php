addd59d69540ca1bfa266129687e8e4865441e13
Benzamin Basher <benzamin.basher@widespace.com>
2016-01-06 22:48:35 +0600

    Commit.

--- movierequest/dbConfig.php
+++ movierequest/dbConfig.php
@@ -4,13 +4,7 @@
  */
 
 define('DB_SERVER'		, 'localhost');
-define('DB_USER'			, 'hackathon');
-define('DB_PASS'			, '2015');
-define('DB_NAME'			, 'wmdb');
-
-
-// define('DB_SERVER'		, 'localhost');
-// define('DB_USER'			, 'benzamin');
-// define('DB_PASS'			, 'abc123');
-// define('DB_NAME'			, 'movieRequestDB');
+define('DB_USER'			, 'benzamin');
+define('DB_PASS'			, 'abc123');
+define('DB_NAME'			, 'movieRequestDB');
 ?>
